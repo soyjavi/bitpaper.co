c07337493df03b68ed4c0176a297cf87033f5e53
soyjavi <hello@soyjavi.com>
2019-11-04 18:17:19 +0700

    First version of demo

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,1 +1,13 @@
+import React, { Fragment, PureComponent } from 'react';
+import ReactDOM from 'react-dom';
+
+class InvoiceDemo extends PureComponent {
+  render() {
+    return (
+      <div>hola</div>
+    );
+  }
+}
 console.log('home.js');
+
+// ReactDOM.render(<FormRegister />, document.getElementById('form'));
