530a2c6cbb8fe0d34f6c930e11c750a0dd2bc452
soyjavi <hello@soyjavi.com>
2019-11-08 19:12:40 +0700

    New version for service /transaction

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -1,18 +1,31 @@
 import fetch from 'node-fetch';
+import Storage from 'vanilla-storage';
+
+import { ERROR } from '../common';
 
 const BASE_URL = 'https://blockstream.info/api/address';
 
-export default async ({ props: { address } }, res) => {
-  /*
-  @TODO
-  1. Find the invoicement using the id && address and username (domain)
-  2. ask blockstream
-  3. compare if invoice is in memorypool or already confirmed (using the amount)
-  */
-  const response = await fetch(`${BASE_URL}/${address}`).catch((error) => console.error(error));
+/*
+@TODO
+1. Find the invoicement using the id && address and username (domain)
+2. ask blockstream
+3. compare if invoice is in memorypool or already confirmed (using the amount)
+*/
 
-  if (!response) throw Error('Something wrong');
-  const json = await response.json();
+export default async ({ props: { domain, invoice: id } }, res) => {
+  const user = new Storage({ filename: domain });
+  const invoice = user.get('invoices').findOne({ id });
 
-  res.json(json);
+  if (!invoice) return ERROR.NOT_FOUND(res);
+  let { tx } = invoice;
+
+  if (!tx) {
+    const { satoshis, address } = invoice;
+    const response = await fetch(`${BASE_URL}/${address}`).catch((error) => console.error(error));
+
+    if (!response) throw Error('Something wrong');
+    tx = await response.json();
+  }
+
+  res.json(tx);
 };
