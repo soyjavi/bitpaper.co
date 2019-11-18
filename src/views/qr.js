ebe13b8abfa1c34b79840d04c9900be097ba1b5b
soyjavi <hello@soyjavi.com>
2019-11-18 10:17:15 +0700

    Use pipe for images within tor

--- src/views/qr.js
+++ src/views/qr.js
@@ -1,17 +1,15 @@
 import tor from 'tor-request';
 
-const BASE_URL = 'http://chart.googleapis.com/chart?cht=qr&chs=256x256&chld=H|0&chl=';
+const BASE_URL = 'https://chart.googleapis.com/chart?cht=qr&chs=256x256&chld=H|0&chl=';
 
 export default ({ params: { address, amount } }, res) => {
-  tor.request(`${BASE_URL}bitcoin:${address}?amount=${amount}`, (error, response) => {
-    if (response.statusCode === 200) {
-      const { headers = {} } = response;
-
-      res.writeHead(200, { 'Content-Type': headers['content-type'] });
-      response.pipe(res);
-    } else {
-      res.writeHead(response.statusCode);
-      res.end();
-    }
-  });
+  tor.request(
+    { url: `${BASE_URL}bitcoin:${address}?amount=${amount}`, strictSSL: true },
+    (error, { statusCode }) => {
+      if (error) {
+        res.writeHead(statusCode);
+        res.end();
+      }
+    },
+  ).pipe(res);
 };
